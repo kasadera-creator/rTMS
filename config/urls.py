39e2e9375b3c58eb2c1af852af142abaf3a910f6
kasadera-creator <ekuni.poke@gmail.com>
2025-12-12 23:32:43 +0900

    chore: format config urls

--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,10 @@
+# config/urls.py
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    # トップページに来たらダッシュボードへ転送
-    path('', RedirectView.as_view(url='/app/dashboard/', permanent=False)),
-    
-    # アプリのURLを '/app/' 以下に割り当て
-    path('app/', include('rtms_app.urls')),
-    
-    path('admin/', admin.site.urls),
+    path("", RedirectView.as_view(url="/app/dashboard/", permanent=False)),
+    path("app/", include("rtms_app.urls")),
+    path("admin/", admin.site.urls),
 ]
