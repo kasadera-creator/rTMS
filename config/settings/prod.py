c0d6dced6375f166d3a33e2a799b4917545d111d
kasadera-creator <ekuni.poke@gmail.com>
2025-12-19 07:05:16 +0900

    Update prod.py

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,4 +1,11 @@
 from .base import *
+import os
 
 # Production specific settings can go here
 # But most are handled via environment variables in base.py
+
+# Automatically add Render host to ALLOWED_HOSTS
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    if RENDER_EXTERNAL_HOSTNAME not in ALLOWED_HOSTS:
+        ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
