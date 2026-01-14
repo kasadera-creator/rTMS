a9d371e7da1672c00c28fe3a47f36325bdb35d9e
kasadera-creator <ekuni.poke@gmail.com>
2026-01-14 20:22:13 +0900

    Update dev.py

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,4 +1,4 @@
 from .base import *
 
 DEBUG = True
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "rtms.local", ",seichiryo.jp"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "rtms.local", "seichiryo.jp", "www.seichiryo.jp"]
