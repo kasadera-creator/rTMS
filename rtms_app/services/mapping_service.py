6e527cb9f44c61b327ae421709f8e5cae526fe72
kasadera-creator <ekuni.poke@gmail.com>
2026-08-20 22:53:36 +0900

    chore: remove confirmed unused files

--- rtms_app/services/mapping_service.py
+++ rtms_app/services/mapping_service.py
@@ -1,14 +1,1 @@
-from rtms_app.models import MappingSession
 
-
-def get_latest_mt_percent(patient):
-    """Return latest resting MT% from mapping sessions for initial treatment defaults."""
-    mapping = (
-        MappingSession.objects
-        .filter(patient=patient)
-        .order_by('-date', '-id')
-        .first()
-    )
-    if not mapping:
-        return None
-    return mapping.resting_mt
